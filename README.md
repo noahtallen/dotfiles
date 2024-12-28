d4b96c9d7992630ddc723b2b695853814a96693f
Noah Allen <noahtallen@gmail.com>
2024-12-27 19:59:43 -0800

    Add brew installation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# dotfiles
+# Noah's Dotfiles
+
+This repo stores my dotfiles using GNU stow.
+
+## Adding things to the repo
+1. Make a top-level directory in this repo for the new entry, and then subdirectories that match the system location from $HOME.
+2. Copy existing files to this new directory.
+3. Run `stow foo --target="$HOME" --adopt` to link the files from the repo.
+
+For example,`./ssh/.ssh/` can hold the SSH `config` file, and then `stow ssh --target="$HOME"` will link it (and every other file under `./ssh/`) to the correct spot in your home directory (~/.ssh/config). This approach lets you group files related to the same feature, even though they might actually live in different parts of the filesystem.
+
+## Update brew packages and applications:
+* Update the list programs to install by running `brew bundle dump` in this directory.
+* To check if you have all programs installed, run `brew bundle check`.
+* To install missing programs, run `brew bundle install`
--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # TODO:
-# - Install Brew
 # - Mac settings
-# - Brew stuff
-# - Cascadia Code NF
 
-if ! [-x command -v brew 2>&1 >/dev/null] ; then
+if ! type brew > /dev/null ; then
     echo "Installing brew..."
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Brew already installed."
 fi
+
+# Add any missing deps:
+echo
+echo "Install missing brew programs"
+brew bundle install
 
 # Add configurations:
 stow config --target="$HOME"
@@ -17,12 +22,15 @@ stow shell --target="$HOME"
 stow git --target="$HOME"
 stow ssh --target="$HOME"
 stow fonts --target="$HOME"
+stow gnupg --target="$HOME"
 
 # OMZ:
 if ! [ -d ~/.oh-my-zsh ]; then
-  echo "Installing Oh My ZSH.."
+  echo "Installing Oh My ZSH."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
   # And plugins!
   git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-bat
   git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+else
+    echo "OMZ already installed."
 fi
