796733aba0f732681081a598e4ac0a0a43206683
Noah Allen <noahtallen@gmail.com>
2025-03-15 09:21:54 -0700

    change to use ghostty and not kitty

--- keyboard-shortcuts/.hammerspoon/init.lua
+++ keyboard-shortcuts/.hammerspoon/init.lua
@@ -1,6 +1,6 @@
 hs.hotkey.bind({ 'ctrl, shift' }, 'return',
-    function()                                   -- change your own hotkey combo here, available keys could be found here:https://www.hammerspoon.org/docs/hs.hotkey.html#bind
-        local BUNDLE_ID = 'net.kovidgoyal.kitty' -- more accurate to avoid mismatching on browser titles
+    function()                                    -- change your own hotkey combo here, available keys could be found here:https://www.hammerspoon.org/docs/hs.hotkey.html#bind
+        local BUNDLE_ID = 'com.mitchellh.ghostty' -- more accurate to avoid mismatching on browser titles
 
         function getMainWindow(app)
             -- get main window from app
@@ -11,11 +11,11 @@ hs.hotkey.bind({ 'ctrl, shift' }, 'return',
             return win
         end
 
-        function moveWindow(kitty, space, mainScreen)
+        function moveWindow(ghostty, space, mainScreen)
             -- move to main space
-            local win = getMainWindow(kitty)
+            local win = getMainWindow(ghostty)
             if win:isFullScreen() then
-                hs.eventtap.keyStroke('fn', 'f', 0, kitty)
+                hs.eventtap.keyStroke('fn', 'f', 0, ghostty)
             end
             --   This moves Kitty to the top third of the screen, which I don't need right now.
             --   winFrame = win:frame()
@@ -26,7 +26,7 @@ hs.hotkey.bind({ 'ctrl, shift' }, 'return',
             --   win:setFrame(winFrame, 0)
             hs.spaces.moveWindowToSpace(win, space)
             if win:isFullScreen() then
-                hs.eventtap.keyStroke('fn', 'f', 0, kitty)
+                hs.eventtap.keyStroke('fn', 'f', 0, ghostty)
             end
             win:focus()
         end
